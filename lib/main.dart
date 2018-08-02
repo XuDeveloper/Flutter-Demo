a2ae7fc5e120fe3c7b39fb80aa6e0558f9aded02
XuDeveloper <280745397@qq.com>
2018-08-02 22:54:42 +0800

    [UPDATE]更新demo(4)

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,24 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
 
-  // todo 第五步
   final _saved = new Set<WordPair>();
 
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return new ListTile(
+            title: new Text(pair.asPascalCase, style: _biggerFont));
+      });
+      final divided = ListTile.divideTiles(context: context, tiles: tiles)
+          .toList();
+      return new Scaffold(
+        appBar: new AppBar(title: new Text("saved"),),
+        body: new ListView(children: divided,),
+      );
+    }));
+  }
 
   Widget _buildSuggestions() {
     return new ListView.builder(
@@ -37,11 +51,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -50,6 +79,9 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("test"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
